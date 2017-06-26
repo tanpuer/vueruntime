16b7281b732384b42aa88d87ee3f8e555892a361
templechen <templechen@qiyi.com>
2017-06-26 16:47:31 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,42 @@
+<template>
+  <panel class="hello">
+    <h1>{{ msg }}</h1>
+    <world></world>
+  </panel>
+</template>
+
+<script>
+  import World from './World.vue';
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  components: {
+      World
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/World.vue
@@ -0,0 +1,19 @@
+/**
+ * Created by cw on 2017/6/26.
+ */
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'world',
+    data () {
+      return {
+        msg: 'Hello World'
+      }
+    }
+  }
+</script>
